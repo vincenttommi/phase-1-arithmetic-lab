fcd4f0eaca46b7cc772667e2ae3a58283ed34db2
vincenttommi <vincenttommi@gmail.com>
2023-03-21 05:26:18 +0300

    arithmetic

--- index.js
+++ index.js
@@ -1,2 +1,26 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+
+const multiply = num1 * num2;
+
+console.log(multiply);
+
+
+const random = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER) + 1;
+
+
+let num3 = 10;
+let num4 = 3;
+let mod = (num3 % num4);
+
+let c=mod+4;
+
+console.log(c);
+
+
+const numbers = [5, 10, 20, 15, 7];
+const max = Math.max(...numbers);
+
+console.log(max);
